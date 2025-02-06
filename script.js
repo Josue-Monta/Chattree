d8cbe242d79fb385d0773b9eefd405432b96aa19
Josue <josuejeral100@gmail.com>
2025-02-06 11:54:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => { 
     cargarMensajes();
+    activarModoOscuro();
 });
 
 document.getElementById("darkModeToggle").addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+    localStorage.setItem("dark-mode", document.body.classList.contains("dark-mode"));
 });
+
+function activarModoOscuro() {
+    if (localStorage.getItem("dark-mode") === "true") {
+        document.body.classList.add("dark-mode");
+    }
+}
 
 let memoriaConversacion = JSON.parse(localStorage.getItem("memoria")) || [];
 
